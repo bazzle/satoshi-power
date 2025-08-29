1fae4505bca62afda5bbed9ad18085e154d222c5
Barry Richards <barry@br-web.me>
2025-08-29 13:54:28 +0100

    Add footer

--- src/app/ui/modules/Footer/index.jsx
+++ src/app/ui/modules/Footer/index.jsx
@@ -1,7 +1,19 @@
+import styles from './Footer.module.scss'
+import NostrCopy from '@/app/ui/components/NostrCopy'
+
 function Footer(){
 	return (
-		<footer>
-			<p>This is the footer</p>
+		<footer className={styles.footer}>
+			<div className="row">
+				<div className="row-container--wider">
+					<div className={styles.footer__container}>
+						<p>
+							Any feedback? please DM me: <a href="https://twitter.com/bazzle">Bazzle</a><br/>
+							<NostrCopy/>
+						</p>
+					</div>
+				</div>
+			</div>
 		</footer>
 	)
 }
