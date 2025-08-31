bf4a215c3f966e2bd90016b80047cecab4751b25
Barry Richards <barry@br-web.me>
2025-08-31 11:50:12 +0100

    Add footer and nostr copy component

--- src/app/ui/modules/Footer/index.jsx
+++ src/app/ui/modules/Footer/index.jsx
@@ -8,9 +8,9 @@ function Footer(){
 				<div className="row-container--wider">
 					<div className={styles.footer__container}>
 						<p>
-							Any feedback? please DM me: <a href="https://twitter.com/bazzle">Bazzle</a><br/>
-							<NostrCopy/>
+							Any feedback? please <a href="https://x.com/bazzle" title="X account">DM me on X – @bazzle</a>
 						</p>
+						<p>Or find me on nostr: <NostrCopy/></p>
 					</div>
 				</div>
 			</div>
