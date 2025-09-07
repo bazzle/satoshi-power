af634cd5036797054b3a300085a5bd11b3dcf700
Barry Richards <barry@br-web.me>
2025-09-07 22:59:14 +0100

    Updates to page template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/app/ui/modules/Header";
 import Footer from "@/app/ui/modules/Footer";
 import About from "@/app/ui/modules/About";
+import { Grid } from "@/app/ui/modules/Grid";
 
 export default function Home() {
 	return (
 		<>
 			<Header/>
 			<main>
-				<p>Main here</p>
+				<Grid/>
 				<About/>
 			</main>
 			<Footer/>
