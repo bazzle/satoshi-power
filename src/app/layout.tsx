b71ff63d1c2ad935b50e51a227a83bff5ea12cfb
Barry Richards <barry@br-web.me>
2025-09-18 17:19:53 +0100

    Move font import to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children } : Readonly<{ children: React.ReactNode; }>) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+				/>
+			</head>
 			<body>{children}</body>
 		</html>
 	);
