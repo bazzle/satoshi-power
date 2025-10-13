54c4e4295db00f6431caf7ad36be2ea79b764839
Barry Richards <barry@br-web.me>
2025-10-13 23:34:40 +0100

    Some minor styling and cleanup

--- src/app/ui/modules/Grid/index.jsx
+++ src/app/ui/modules/Grid/index.jsx
@@ -3,7 +3,7 @@ import CurrencyItem from "./CurrencyItem";
 import styles from "./Grid.module.scss";
 import { LiveDataContext}  from "@/app/data/LiveDataContext";
 import { SnapshotsDataContext } from "@/app/data/SnapshotsDataContext";
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 
 export function Grid(){
 
@@ -13,7 +13,11 @@ export function Grid(){
 	console.log(snapshots);
 
 	if (liveData === null || snapshots === undefined){
-		return <p className="flex-center padding">Loading...</p>
+		return (
+			<div className={styles.loader}>
+				<p className="flex-center padding">Loading...</p>
+			</div>
+		)
 	} else {
 		return (
 			<ul className={styles.grid}>
