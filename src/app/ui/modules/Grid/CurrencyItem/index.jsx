85e7cfdcf1aaf8a50afce17a12b87c5a85f441f9
Barry Richards <barry@br-web.me>
2025-10-15 01:06:03 +0100

    Renamed some files

---
+++ src/app/ui/modules/Grid/CurrencyItem/index.jsx
@@ -0,0 +1,55 @@
+'use client';
+
+import styles from './currencyItem.module.scss';
+import icons from '@/app/ui/misc/Icons';
+import Link from 'next/link';
+
+function CurrencyItem({itemObj}){
+
+	const classesArray = [
+		styles.currencyItem,
+		itemObj.smallUnitKilled ? styles["currencyItem--subUnitKilled"] : '',
+		itemObj.mainUnitKilled ? styles["currencyItem--mainUnitKilled"] : '',
+	]
+	const classes = classesArray.join(' ');
+
+	const DomPercentageBar = () => {
+		const width = itemObj.percentage > 100 ? 100 : itemObj.percentage;
+		return (
+			<span style={{width: `${width}%`}} className={styles.currencyItem__percentageBar}></span>
+		)
+	};
+
+	const DomTextString = () => {
+		const name = itemObj.displayName;
+		const itemTextString = itemObj.percentage < 1 ? `${name} <1%` : `${name} ${itemObj.percentage}%`;
+		return (
+			<span className={styles.currencyItem__text}>
+				{itemTextString}
+			</span>
+		)
+	}
+
+	const DomIcons = () => {
+		const Icon = <div className={styles.currencyItem__icon}>{icons.skull}</div>
+		return (
+			<div className={styles.currencyItem__statusContainer}>
+				{ ((itemObj.subUnitKilled) ? Icon : '' )}
+				{ ((itemObj.mainUnitKilled) ? Icon : '' )}
+			</div>
+		)
+	}
+
+	
+	return (
+		<li className={classes}>
+			<Link href="/currency/naira">
+				{DomPercentageBar()}
+				{DomTextString()}
+				{DomIcons()}
+			</Link>
+		</li>
+	)
+}
+
+export default CurrencyItem
