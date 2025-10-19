eeead61faf30080af853a79d613de1f78def700d
Barry Richards <barry@br-web.me>
2025-10-19 20:44:28 +0100

    Pull in promise data and use as page slugs

--- src/app/currency/[slug]/page.jsx
+++ src/app/currency/[slug]/page.jsx
@@ -1,50 +1,22 @@
 import Header from "@/app/ui/modules/Header";
 import Footer from "@/app/ui/modules/Footer";
 import About from "@/app/ui/modules/About";
+import getDataPromise from "@/app/data/getData";
+
+
 
 export const dynamicParams = false;
 
-const slugs = [
-	"czech-koruna",
-	"thai-baht",
-	"new-taiwan-dollar",
-	"turkish-lira",
-	"british-pound",
-	"swiss-franc",
-	"russian-ruble",
-	"euro",
-	"indian-rupee",
-	"united-states-dollar",
-	"icelandic-krona",
-	"singapore-dollar",
-	"canadian-dollar",
-	"australian-dollar",
-	"japanese-yen",
-	"new-zealand-dollar",
-	"hungarian-forint",
-	"polish-zloty",
-	"romanian-leu",
-	"brazilian-real",
-	"danish-krone",
-	"chinese-renminbi-yuan",
-	"hong-kong-dollar",
-	"swedish-krona",
-	"chilean-peso",
-	"south-korean-won",
-	"argentine-peso",
-	"nigerian-naira"
-];
-
 // Tell Next which static paths to build
 export async function generateStaticParams() {
-	return slugs.map((slug) => (
-		{ slug }
-	))
+	const data = await getDataPromise();
+	const dataArr = Object.values(data);
+	return dataArr.map((item) => ({ slug: item.displayNameSlug }));
 }
 
-async function Page({ params }) {
+function Page({ params }) {
 	try {
-		let {slug} = await params;
+		const { slug } = params;
 
 		return (
 			<>
