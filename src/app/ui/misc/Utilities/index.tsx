79daac28d2d4f45b61c594f857e0850b3e62d1d8
Barry Richards <barry@br-web.me>
2025-10-20 01:38:36 +0100

    Add slugify to utilities

--- src/app/ui/misc/Utilities/index.tsx
+++ src/app/ui/misc/Utilities/index.tsx
@@ -1,9 +1,14 @@
+import slugify from "slugify";
+
 const Utilities = {
     getPercentage : (unitPrice:number) => {
         return Math.floor(unitPrice * 100);
     },
 	removeLastWord : (str:string) => {
 		return str.trim().split(" ").slice(0, -1).join(" ");
+	},
+	slugify : (str:string) => {
+		return slugify(str, { lower: true, strict: true });
 	}
 }
 
