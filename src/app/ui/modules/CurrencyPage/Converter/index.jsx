8530bcffbee536d7e17b01ff05ec6c7b51c3d745
Barry Richards <barry@br-web.me>
2025-10-26 16:38:45 +0000

    Add logic for outputs

--- src/app/ui/modules/CurrencyPage/Converter/index.jsx
+++ src/app/ui/modules/CurrencyPage/Converter/index.jsx
@@ -8,14 +8,11 @@ import { RadioGroup } from '@ark-ui/react/radio-group'
 function Converter({convCurrency}){
 	const [mode, setMode] = useState('sats');
 	const [inputNumber, setInputNumber] = useState(1);
-	const [outputNumber, setOutputNumber] = useState();
+	const [inputNumberFormDisplay, setInputNumberFormDisplay] = useState();
 	const [outputString, setOutputString] = useState('');
-
-	// console.log(NumberInput)
 
 	// Run on load and when input number changes
 	useEffect(()=>{
-		console.log(mode);
 		let rawNum
 		let outputValue
 		let outputString
@@ -23,18 +20,17 @@ function Converter({convCurrency}){
 		if(mode === 'sats'){
 			rawNum = inputNumber * convCurrency.satPrice;
 			outputValue = Number(Number(rawNum).toFixed(2));
-			setOutputNumber(outputValue);
 			outputString = (
-				<p>{inputNumber} Satoshi's = {currencyString} {outputValue}</p>
+				<p>{inputNumber.toLocaleString()} Satoshi's = {currencyString} {outputValue}</p>
 			)
 		} else if(mode === 'fiat') {
 			rawNum = inputNumber / convCurrency.satPrice;
 			outputValue = Number(Number(rawNum).toFixed(2));
-			setOutputNumber(outputValue);
 			outputString = (
-				<p>{inputNumber} {currencyString} = {outputValue} Satoshi's</p>
+				<p>{inputNumber} {currencyString} = {inputNumber.toLocaleString()} Satoshi's</p>
 			)
 		}
+		setInputNumberFormDisplay(inputNumber);
 		setOutputString(outputString);
 	},[inputNumber, mode])
 
@@ -43,39 +39,35 @@ function Converter({convCurrency}){
 	return(
 		<div className={styles.converter}>
 			<form className={styles.converter__form}>
-				<div className={styles.converter__inputNumber}>
-					<NumberInput.Root
-						min={0}
-						defaultValue={1}
-						onValueChange={(num) => setInputNumber(num.valueAsNumber)}
-					>
-						<NumberInput.Label>Input number</NumberInput.Label>
-						<NumberInput.Input />
-						<NumberInput.Control>
-							<NumberInput.IncrementTrigger>▲</NumberInput.IncrementTrigger>
-							<NumberInput.DecrementTrigger>▼</NumberInput.DecrementTrigger>
-						</NumberInput.Control>
-					</NumberInput.Root>
-
-				</div>
-				<div className={styles.converter__inputRadio}>
+				<NumberInput.Root
+					min={0}
+					step={1}
+					value={inputNumberFormDisplay}
+					onValueChange={(num) => setInputNumber(num.valueAsNumber)}
+				>
+					<NumberInput.Label htmlFor="inputNum">Input number</NumberInput.Label>
+					<NumberInput.Input id="inputNum" />
+					<NumberInput.Control>
+						<NumberInput.IncrementTrigger>▲</NumberInput.IncrementTrigger>
+						<NumberInput.DecrementTrigger>▼</NumberInput.DecrementTrigger>
+					</NumberInput.Control>
+				</NumberInput.Root>
 
 				<RadioGroup.Root
 					onValueChange={(mode) => mode.value === "Sats" ? setMode('sats') : setMode('fiat') }
 					defaultValue="Sats"
 				>
-					<RadioGroup.Label>Unit</RadioGroup.Label>
+					<RadioGroup.Label htmlFor="inputRadio">Unit</RadioGroup.Label>
 					<RadioGroup.Indicator />
 					{unitChoices.map((unitChoice) => (
-						<RadioGroup.Item key={unitChoice} value={unitChoice}>
+						<RadioGroup.Item id="inputRadio" key={unitChoice} value={unitChoice}>
 							<RadioGroup.ItemText>{unitChoice}</RadioGroup.ItemText>
 							<RadioGroup.ItemControl />
 							<RadioGroup.ItemHiddenInput />
 						</RadioGroup.Item>
 					))}
 				</RadioGroup.Root>
-
-				</div>
+				
 			</form>
 			<div className={styles.converter__output}>
 				{outputString}
