53503d926ceff758972a1032a32a46190d4e092e
Barry Richards <barry@br-web.me>
2026-04-19 16:36:15 +0100

    Formatting update

--- src/app/currency/[slug]/page.jsx
+++ src/app/currency/[slug]/page.jsx
@@ -1,27 +1,27 @@
-import Header from "@/app/ui/modules/Header";
-import Footer from "@/app/ui/modules/Footer";
-import About from "@/app/ui/modules/About";
-import getDataPromise from "@/app/data/getData";
-import CurrencyPage from "@/app/ui/modules/CurrencyPage";
+import Header from '@/app/ui/modules/Header'
+import Footer from '@/app/ui/modules/Footer'
+import About from '@/app/ui/modules/About'
+import getDataPromise from '@/app/data/getData'
+import CurrencyPage from '@/app/ui/modules/CurrencyPage'
 
-export const dynamicParams = false;
+export const dynamicParams = false
 
 // Tell Next which static paths to build
 export async function generateStaticParams() {
-	const data = await getDataPromise();
-	const dataArr = Object.values(data);
-	return dataArr.map((item) => ({ slug: item.currencyCodeSlug }));
+	const data = await getDataPromise()
+	const dataArr = Object.values(data)
+	return dataArr.map((item) => ({ slug: item.currencyCodeSlug }))
 }
 
 async function Page({ params }) {
 	try {
-		const { slug } = await params;
+		const { slug } = await params
 		return (
 			<>
 				<Header/>
 				<main className="main">
 					<div className="row row--nomargin-top main__content">
-                		<div className="row-container">
+						<div className="row-container">
 							<CurrencyPage slug={slug} />
 						</div>
 					</div>
@@ -34,16 +34,16 @@ async function Page({ params }) {
 		)
 	}
 	catch(error) {
-		console.error(error);
+		console.error(error)
 	}
 }
 
 export async function generateMetadata({ params }) {
-	const { slug } = await params;
-	const data = await getDataPromise();
-	const currencyObj = data[slug.toUpperCase()];
+	const { slug } = await params
+	const data = await getDataPromise()
+	const currencyObj = data[slug.toUpperCase()]
 
-	if (!currencyObj) return { title: "Satoshi Power" };
+	if (!currencyObj) return { title: 'Satoshi Power' }
 
 	const title1 = currencyObj.noSubUnit || currencyObj.subUnitKilled ? currencyObj.demonym : `${currencyObj.subUnitName} /`
 	const title2 = currencyObj.unitNameSingular
@@ -58,13 +58,13 @@ export async function generateMetadata({ params }) {
 			title,
 			description,
 			url,
-			type: "website",
-			siteName: "Satoshi Power",
+			type: 'website',
+			siteName: 'Satoshi Power',
 		},
 		alternates: {
-			canonical: url
-		}
-	};
+			canonical: url,
+		},
+	}
 }
 
-export default Page;
+export default Page
