b44c17a23fcf06e4ecd8a0de63228b8d4490e418
Barry Richards <barry@br-web.me>
2026-04-29 00:20:15 +0100

    Convert to LiveDataContext to TS

---
+++ src/app/data/LiveDataContext.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useState, useEffect, createContext } from 'react'
+import getDataPromise from './getData.ts'
+
+export const LiveDataContext = createContext<{ liveData: any } | null>(null)
+
+export function LiveDataProvider({ children }: { children: React.ReactNode }){
+	const [liveData, setLiveData] = useState<any>(null)
+
+	useEffect(() => {
+		async function getData(){
+			const data = await getDataPromise()
+			setLiveData(data)
+		}
+		getData()
+	}, [])
+
+	return (
+		<LiveDataContext.Provider value={{ liveData }}>
+			{children}
+		</LiveDataContext.Provider>
+	)
+
+}
